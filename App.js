28d02ba657f21d60b595c59250aa3e0224c87ce3
Menna Shazly <mnnshazly@gmail.com>
2021-08-05 18:27:48 +0200

    Simple Counter App

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  onIncrement = () => {
+    this.setState({
+      count: this.state.count +1
+    })
+  }
+
+  onDecrement = () => {
+    this.setState({
+      count: this.state.count -1
+    })
+  }
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Button style={styles.button} onPress={this.onIncrement}  title="+" />
+        <Text style={styles.txt}> {this.state.count} </Text>
+        <Button style={styles.button} onPress={this.onDecrement} title="-"/>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  txt: {
+    color: 'white',
+    fontSize: 200,
+    margin: 30,
+  },
+
+  button: {
+    color: 'red',
+    marginTop: 20,
+     padding: 20,
   },
 });
